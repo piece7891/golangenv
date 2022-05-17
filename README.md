554748a5b4e0b51b9e2463f9bfa61e0787928aea
JW <pieceof@ymail.com>
2022-05-16 20:49:34 -0400

    Update code with pure go code

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 
 * Change log:
 > 220515    Jack Wong Initial
+> 220516    Jack Wong Update code with pure go code
+
+~~~
+* Compiling step
+$ go build golangenv.go
+~~~
--- golangenv.go
+++ golangenv.go
@@ -1,37 +1,15 @@
 package main
- 
- import (
-  "fmt"
-  "io/ioutil"
-  "os/exec"
- )
- 
- func main() {
-  cmd := exec.Command("/bin/bash", "-c", `env`)
- 
-  //Create get command output pipeline
-  stdout, err := cmd.StdoutPipe()
-  if err != nil {
-   fmt.Printf("Error:can not obtain stdout pipe for command:%s\n", err)
-   return
-  }
- 
-  //Execute command
-  if err := cmd.Start(); err != nil {
-   fmt.Println("Error:The command is err,", err)
-   return
-  }
- 
-  //Read all outputs
-  bytes, err := ioutil.ReadAll(stdout)
-  if err != nil {
-   fmt.Println("ReadAll Stdout:", err.Error())
-   return
-  }
- 
-  if err := cmd.Wait(); err != nil {
-   fmt.Println("wait:", err.Error())
-   return
-  }
-  fmt.Printf("stdout:\n\n %s", bytes)
- }
+
+import (
+  "os"
+  "fmt"  
+  "strings"  
+)
+
+func main() {	
+	// fetcha all env variables
+	for _, element := range os.Environ() {
+        variable := strings.Split(element, "=")
+        fmt.Println(variable[0],"=",variable[1])		
+    }
+}
